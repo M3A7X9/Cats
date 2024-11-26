761f1f3efc9627016b371c27ce957c14cd0c8b0e
WizZzard <max_1993@inbox.ru>
2024-11-26 22:33:01 +0300

    Добавим изменение размера изображения

--- Cataas.py
+++ Cataas.py
@@ -5,43 +5,34 @@ from io import BytesIO
 
 def load_image(url):
     try:
-        # Отправляем GET-запрос с использованием requests.get()
         response = requests.get(url)
-        # Проверяем успешность запроса (код ответа 200)
         response.raise_for_status()
-        # Читаем байты из ответа в объект BytesIO
         image_data = BytesIO(response.content)
-        # Открываем изображение с помощью PIL
         img = Image.open(image_data)
+        # Изменяем размер изображения
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Ошибка при загрузке изображения: {e}")
         return None
 
 def set_image():
-    # Вызываем функцию для загрузки изображения
     img = load_image(url)
-
     if img:
-        # Устанавливаем изображение в метку
         label.config(image=img)
-        label.image = img
+        label.image = img  # Сохраняем ссылку на изображение
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x480")
+window.geometry("600x520")
 
-# Создаем метку без изображения
-label = Label()
+label = Label(window)
 label.pack()
 
-# Добавляем кнопку для обновления изображения
 update_button = Button(text="Обновить", command=set_image)
 update_button.pack()
 
-url = 'https://cataas.com/cat'
-
-# Вызываем функцию для установки изображения в метку
+url = 'https://cataas.com/cat/cute'
 set_image()
 
 window.mainloop()
