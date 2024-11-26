e74c25145bac6a38efd712a7f70600110fd42c83
WizZzard <max_1993@inbox.ru>
2024-11-26 22:20:17 +0300

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,29 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+# Создаем метку без изображения
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    # Устанавливаем изображение в метку
+    label.config(image=img)
+    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
+    label.image = img
+
+if img:
+    # Устанавливаем изображение в метку
+    label.config(image=img)
+    # Необходимо сохранить ссылку на изображение, чтобы избежать сборки мусора
+    label.image = img
+
+window.mainloop()
